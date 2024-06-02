e8d89a8b33a554ee0470e5a8a4291499244d211f
Nabin Yadav <110272675+ynabin@users.noreply.github.com>
2024-06-02 00:46:33 -0700

    Add files via upload
    find out equal number to other number.

---
+++ greater1.py
@@ -0,0 +1,11 @@
+number = int(input("enter the number"))
+if number == 10:
+    print("number is equal to 10")
+elif number==50:
+    print("number is equal to 50")
+elif number==50:
+    print("number is equal to 50")
+elif number==100:
+    print("number is equal to 100")
+else:
+    print("number is not equal to 10, 50 or 100")
